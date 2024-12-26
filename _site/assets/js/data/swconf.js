2884416a1d01146f6ee8bac9469e64449ed5f56f
Daeho Kwon <daeho.kwon@outlook.com>
2024-12-26 12:36:19 +0900

    Fix

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735183048',resources: [
+    cacheName: 'chirpy-1735184154',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
